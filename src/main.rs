1bf6803083d14e2fa9bf3f7910e586c46d126e57
Julionate <julionate@proton.me>
2026-06-18 14:52:51 -0400

    feat: add first menu prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::stdin;
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Welcome to Task Manager CLI, choose an option");
+        println!("1. Manage Tasks\n2. List Tasks\n3. Save Tasks\n'Exit' to leave");
+        let input = read_input();
+
+        match input.to_lowercase().trim() {
+            "1" => {
+                println!("You choose Manage tasks")
+            }
+            "2" => {
+                println!("You choose list tasks")
+            }
+            "3" => {
+                println!("You choose save tasks")
+            }
+            "exit" => {
+                println!("Closing Task Manager CLI...");
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer).unwrap();
+    println!("You entered: {}", buffer);
+    buffer
 }
